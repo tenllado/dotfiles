88e10c98214497718048d0a19bcf180d831e738e
Christian Tenllado <tenllado@ucm.es>
2019-10-19 00:52:54 +0200

    dynamic-colors

---
+++ dynamic-colors/colorschemes/old/simple.sh
@@ -0,0 +1,31 @@
+# Color Scheme: simple
+# My colorscheme: based on OSX Basic palette for white background with some
+# modifications borrowed from gnome-terminal and my own selection of colors
+foreground="#000000"
+background="#ffffff"
+cursorColor="#000000"
+pointerColor="#000000"
+#black
+color0="#000000"
+color8="#666666"
+#red
+color1="#990000"
+color9="#e50000"
+#green
+color2="#008800"
+color10="#22bb22"
+#yellow
+color3="#aa5500"
+color11="#EDBC1D"
+#blue
+color4="#0000b2"
+color12="#0000ff"
+#magenta
+color5="#b200b2"
+color13="#e500e5"
+#cyan
+color6="#00a6b2"
+color14="#00c6d2"
+#white
+color7="#bfbfbf"
+color15="#e5e5e5"
