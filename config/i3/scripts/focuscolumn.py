692db76f8541315cda58ef5ec9d07deddeab3fae
Christian Tenllado <ctenllado@gmail.com>
2020-05-03 11:52:22 +0200

    focuscolumn options swap & move, config update

--- config/i3/scripts/focuscolumn.py
+++ config/i3/scripts/focuscolumn.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 #
-# if you have two columns at the desktop level this script changes the focus to
-# the other colum, regardless of the layout in the containers of each column
+# This script implements interesting operations for i3 when you work with a two
+# columns layout at the workspace level. It enables three basic operations that
+# you can bind to different keys:
+#
+#   1. whit no options the script changes the focus to the other colum,
+#      regardless of the layout in the containers of each column
+#   2. with -s (swap), the script swaps the focused window with the focus
+#      inactive one in the other column, regardless of the layout in both
+#      columns
+#   3. with -m (move), the script moves the window to the other column,
+#      regardless of the layout in both columns
+#   4. with -m|-s and -a (alternate), the focus is set at the end on the origin
+#      column insted of the destination column
 #
 # GPL License header
 #
@@ -18,19 +29,39 @@
 # You should have received a copy of the GNU General Public License along with
 # this program.  If not, see <https://www.gnu.org/licenses/>.
 #
-# Author: Christian Tenllado Copyright 2020 Christian Tenllado e-mail:
-# ctenllado@gmail.com
+# Author: Christian Tenllado
+# Copyright 2020 Christian Tenllado
+# e-mail: ctenllado@gmail.com
 
-
+import optparse
 from i3ipc import Connection
 
+options = None
+args = None
+
+def parse_options():
+    global options
+    global args
+
+    usage_str = "%prog [-m -s]"
+    parser = optparse.OptionParser(usage_str)
+    parser.add_option("-s", action="store_true", dest="swap", default=False,
+            help="swap with the focus inactive window in the other column")
+    parser.add_option("-m", action="store_true", dest="move", default=False,
+            help="move the window to the other column")
+    parser.add_option("-a", action="store_true", dest="alternate",
+            default=False,
+            help="when swaping or moving focus the other column at the end")
+
+    options, args  = parser.parse_args()
+
 def switch_col(con, focused):
+    global options, args
     if con.layout == 'splith' and len(con.nodes) == 1 and \
             con.nodes[0].layout == 'splith':
         return switch_col(con.nodes[0], focused)
     elif con.layout != 'splith' or len(con.nodes) != 2:
         return
-
 
     if (len(con.nodes[0].nodes) == 0 and con.nodes[0].window == focused.window)\
             or con.nodes[0].find_by_id(focused.id):
@@ -42,10 +73,23 @@ def switch_col(con, focused):
         dest = con.nodes[d]
     else:
         dest = con.nodes[d].find_by_id(con.nodes[d].focus[0])
-    dest.command('focus')
 
+    if options.swap:
+        dest.command('mark --add _quickswap')
+        focused.command('swap container with mark _quickswap')
+        dest.command('unmark _quickswap')
+    elif options.move:
+        if len(dest.nodes) == 0:
+            dest.command('split v')
+        dest.command('mark --add _target')
+        focused.command('move container to mark _target')
+        dest.command('unmark _target')
+
+    if (not options.swap and not options.move) or options.alternate:
+        dest.command('focus')
 
 def main():
+    parse_options()
     cnx = Connection()
     focused = cnx.get_tree().find_focused()
     wsp = focused.workspace()
