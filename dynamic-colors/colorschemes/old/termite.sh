d0b58e81d9d80d7671cc1d3dddb9010af67146fd
Christian Tenllado <ctenllado@gmail.com>
2020-05-25 09:34:21 +0200

    termite dark green colorscheme

---
+++ dynamic-colors/colorschemes/old/termite.sh
@@ -0,0 +1,37 @@
+# Termite dark colorscheme
+
+foreground="#ececec"
+background="#1a1a1a"
+
+#colors
+# black
+color0="#1c1c1c"
+color8="#505050"
+
+# red
+color1="#cd5c5c"
+color9="#e5505f"
+
+# green
+color2="#7a8f65"
+color10="#587925"
+
+# yellow
+color3="#8B4513"
+color11="#e1db3b"
+
+# blue
+color4="#0087af"
+color12="#00607C"
+
+# purple
+color13="#a2798f"
+color5="#715464"
+
+# cyan
+color6="#5f7b8a"
+color14="#005366"
+
+# white
+color7="#887e86"
+color15="#cacaca"
