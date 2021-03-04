c9d942e06c80e6d8db8d2bdeaa40c92a8d5ec4cd
Christian Tenllado <ctenllado@gmail.com>
2021-03-04 16:17:19 +0100

    i3 scripts update/improved/commented

--- config/i3/scripts/disable-standby-fs.py
+++ config/i3/scripts/disable-standby-fs.py
@@ -3,8 +3,8 @@
 from argparse import ArgumentParser
 from subprocess import call
 import i3ipc
-
-i3 = i3ipc.Connection()
+import subprocess as sp
+import signal
 
 parser = ArgumentParser(prog='disable-standby-fs',
         description='''
@@ -16,31 +16,39 @@ parser.add_argument("k", type=int, help="kill signal for i3blocks")
 
 args = parser.parse_args()
 
-def find_fullscreen(con):
-    # XXX remove me when this method is available on the con in a release
-    return [c for c in con.descendents()
-            if c.type == 'con' and c.fullscreen_mode]
+p = sp.Popen("xset q | grep -A 2 Saver | grep timeout | sed -rn 's/[^[:digit:]]*([[:digit:]]+).*/\\1/p'", shell=True, stdout=sp.PIPE)
+sstimeout = p.stdout.read().strip()
+sson = sstimeout != 0
 
-def set_dpms(state):
-    if state:
-        print('setting dpms on')
-        call('xset s {} 0'.format(args.b).split())
-        call('xset +dpms'.split())
-        call('pkill -RTMIN+{} i3blocks'.format(args.k).split())
+i3 = i3ipc.Connection()
+
+def screen_saver(on):
+    if on:
+        #print('enabling screen saver')
+        call('xset s {}'.format(args.b).split())
     else:
-        print('setting dpms off')
+        #print('disabling screen saver')
         call('xset s off'.split())
-        call('xset -dpms'.split())
-        call('pkill -RTMIN+{} i3blocks'.format(args.k).split())
+    #print('done')
+    call('pkill -RTMIN+{} i3blocks'.format(args.k).split())
 
 def on_fullscreen_mode(i3, e):
-    set_dpms(not len(find_fullscreen(i3.get_tree())))
+    if len(i3.get_tree().find_fullscreen()):
+        screen_saver(False)
+    elif sson:
+        screen_saver(True)
 
 def on_window_close(i3, e):
-    if not len(find_fullscreen(i3.get_tree())):
-        set_dpms(True)
+    if not len(i3.get_tree().find_fullscreen()) and sson:
+        screen_saver(True)
 
+def handler(signo, stack):
+    global sson, i3
+    sson = not sson
+    if not len(i3.get_tree().find_fullscreen()):
+        screen_saver(sson)
+
+signal.signal(signal.SIGUSR1, handler)
 i3.on('window::fullscreen_mode', on_fullscreen_mode)
 i3.on('window::close', on_window_close)
-
 i3.main()
