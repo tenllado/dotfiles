9b2b5b45b0856e186c21147407741ac0f5d4d4e7
Christian Tenllado <ctenllado@gmail.com>
2026-07-08 09:54:28 +0200

    i3 addapt polybar to multimonitor and autorandr to new config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,5 +6,11 @@ pkill polybar
 # 2. Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# 3. Launch your bar
-exec polybar i3bar &
+# 3. Detect connected monitors and launch a bar on each one
+if command -v xrandr >/dev/null; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload i3bar &
+  done
+else
+  polybar --reload i3bar &
+fi
